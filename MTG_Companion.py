110cffcb24aaea46efcdb1408fb7ff7b94495774
MarkStein523 <43623292+MarkStein523@users.noreply.github.com>
2018-09-29 11:43:40 -0400

    Added one drop odds
    Added calculations for the odds of drawing a one drop in the opening hand

--- MTG_Companion.py
+++ MTG_Companion.py
@@ -7,8 +7,18 @@ if __name__ == "__main__":
     import math
     deckSize = int(input("Deck size: "))
     numLands = int(input("Number of lands: "))
+    numOneDrops = int(input("Number of 1 mana cards: "))
     
     oddsOfNotDrawingLand = choose(deckSize - numLands, 7) / choose(deckSize, 7)
     oddsOfDrawingLand = 1 - oddsOfNotDrawingLand
     
+    oddsOfNotDrawingOneDrop = choose(deckSize - numOneDrops, 7) / choose(deckSize, 7)
+    oddsOfDrawingOneDrop = 1 - oddsOfNotDrawingOneDrop
+    
     print("Odds of drawing atleast one land: %.2f" % (oddsOfDrawingLand*100))
+    print("Odds of drawing atleast one 1 mana card: %.2f" % (oddsOfDrawingOneDrop*100))
+    
+    notDrawingOneDropAndLand = oddsOfNotDrawingLand + oddsOfNotDrawingOneDrop
+    drawingOneDropAndLand = 1 - notDrawingOneDropAndLand
+    
+    print("Odds of drawing atleast one 1 mana card AND atleast one land: %.2f" % (drawingOneDropAndLand*100))
